fb04d79b6f785e48a153a46ac9179fcfcc951090
bengista <michal.zrcek@involve.cz>
2025-03-26 16:51:56 +0100

    [REMOVE] Remove creating new migration

--- resources/views/livewire/migration/read.blade.php
+++ resources/views/livewire/migration/read.blade.php
@@ -2,8 +2,6 @@
 
     @include('migrator::message')
 
-    @livewire('migrator::livewire.migration.create')
-
     <div class="mt-8 flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
